b8464e646b55d30b92d306e1e209735123e83d08
Hyde <108911977+Hyaxon@users.noreply.github.com>
2026-03-26 19:02:13 -0500

    Finalized documentation and fixes. Co-authored-by: Jakob238 linenberger.jakob456@gmail.com

--- project2/launch/tb4_sim_launch.py
+++ project2/launch/tb4_sim_launch.py
@@ -34,11 +34,6 @@ def generate_launch_description():
         executable='project2_controller',
         name='project2_controller',
         output='screen',
-        #parameters=[{
-            #'linear_vel': 0.1,
-            #'angular_vel': 0.2,
-            #'distance_threshold': 0.3048
-        #}]
     )
 
     return LaunchDescription([
